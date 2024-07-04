57c0a50ee244f19f3dde33fd390b1a74e8e4e49b
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-07-04 23:38:27 +0200

    add map method

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-//forEach Methods
 const arr = [1, 2, 3, 4];
+
+// forEach Methods
 arr.forEach((num, index, arr) =>
   console.log(`Item of ${index} index = ${num}`)
 );
+
+// map Methods
+const newArr = arr.map((num, index, arr) => num * 2);
+console.log(newArr);
