0261b94995c3f3dc328e401aa9dbb67268f0f14d
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-07-07 23:12:25 +0200

    add reduce method

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const arr = [1, 2, 3, 4];
 
-// forEach Methods
+// forEach Method
 arr.forEach((num, index, arr) =>
   console.log(`Item of ${index} index = ${num}`)
 );
 
-// map Methods
+// map Method
 const newArr = arr.map((num, index, arr) => num * 2);
 console.log(newArr);
 
-// filter Methods
+// filter Method
 
 const newArrTwo = arr.filter((num, i, arr) => num > 3);
 console.log(newArrTwo);
+
+// reduce Method
+const sumOfArr = arr.reduce((acc, element, i, arr) => acc + element, 0);
+console.log(sumOfArr);
